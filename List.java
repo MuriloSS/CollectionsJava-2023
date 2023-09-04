8b309e47b6e60dabbcfd782fc65605c36860ee9c
UNeB - Murilo Santos <murilo.santos@adventistas.org>
2023-09-04 17:19:43 -0300

    Initial commit

---
+++ List.java
@@ -0,0 +1,11 @@
+public class List {
+    public static void main(String[] args) {
+        //TODO Lista de Atividades e exercicios práticos
+
+        //_Item_ Crie uma arrayList
+
+
+        System.out.println();
+    }
+
+}
